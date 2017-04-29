1be43ec06239047f8961760dbca65cf59c15f86c
evanbowman <ebowman@bu.edu>
2017-04-29 19:01:43 -0400

    double error tests for Hamming decoder

--- SubBytes.v
+++ SubBytes.v
@@ -145,7 +145,7 @@ module SubBytesHammingTestBench();
    wire [7:0] out;
    wire [3:0] w, S;
    reg [11:0] checkerInput;
-   reg        i, j;
+   reg [8:0]  i, j;
    SubBytes sb(in, out);
    SubBytesHammingPredictor sbhp(in, w);
    SubBytesHammingChecker sbhc(checkerInput, S);
@@ -169,7 +169,6 @@ module SubBytesHammingTestBench();
       repeat (256) begin
          i = 0;
          repeat (12) begin
-            in = 0;
             #1;
             checkerInput = {out, w};
             checkerInput[i] = ~checkerInput[i];
@@ -187,7 +186,20 @@ module SubBytesHammingTestBench();
       in = 0;
       $display("\nSyndrome tests for two errors...");
       repeat (256) begin
-         
+         for (i = 0; i < 12; i = i + 1) begin
+            for (j = 0; j < i; j = j + 1) begin
+               #1;
+               checkerInput = {out, w};
+               checkerInput[i] = ~checkerInput[i];
+               checkerInput[j] = ~checkerInput[j];
+               #1;
+               if (S == 0) begin;
+                  $display("Error: double bit error somehow masked");
+                  $finish;
+               end;
+            end
+         end
+         in = in + 1;
       end
       $display("Passed!");
    end
